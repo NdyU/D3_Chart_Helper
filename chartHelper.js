3090a7aa83df079501dc9ee16f73b7e8fd02ffda
Andy Yu <andyyu199623@yahoo.com>
2017-01-22 21:33:35 -0500

    Changed to X and Y graph

--- chartHelper.js
+++ chartHelper.js
@@ -2,13 +2,8 @@ var test = function() {
 	this.x = 1;
 };
 
-var scatterGraph = function(data, cWidth, cHeight, g, container) {
-  var margin = {
-    left: 30,
-    right: 10,
-    top: 10,
-    bottom: 30
-  }
+var scatterYGraph = function(data, margin, cWidth, cHeight, g, container) {
+
   
   var containerHeight = cHeight - (margin.top + margin.bottom);
   var containerWidth = cWidth - (margin.left + margin.right);
@@ -16,6 +11,12 @@ var scatterGraph = function(data, cWidth, cHeight, g, container) {
   var gap = g;
   var width = (containerWidth - (dataSize*gap))/dataSize;
   var max, min;
+
+  console.log("Container Width: " + containerWidth);
+  console.log("Container Height: " + containerHeight);
+  console.log("Gap: " + gap, "Total Gap: " + (dataSize*gap));
+  console.log("Width: " + width, "Total width: " + (dataSize * width) );
+  console.log(margin);
 
   for(var i = 0; i < dataSize; i++) {
     if(max) {
@@ -36,48 +37,129 @@ var scatterGraph = function(data, cWidth, cHeight, g, container) {
   }
 
   var dataScale = d3.scaleLinear()
-    .domain([min, max])
-    .range([margin.left, containerHeight-margin.right]);
+    .domain([max, min])
+    .range([margin.top+(width/2), containerHeight - (margin.bottom+width/2)]);
 
-  var newData = [];
-  for(var i = 0; i < dataSize; i++) {
-    newData.push(dataScale(data[i]));
-  }
-
-  containerHeight += width;
+  var rangeScale = d3.scaleLinear()
+    .domain([0, dataSize])
+    .range([margin.left+(width/2), containerWidth-margin.right])
   
   var svgContainer = d3.select(container).append("svg")
     .attr("width", containerWidth + margin.left + margin.right)
     .attr("height", containerHeight + margin.bottom+margin.top);
 
   var circles = svgContainer.selectAll("circle")
-  .data(newData)
+  .data(data)
   .enter()
   .append("circle");
     
   var circleAttributes = circles
-  .attr("cx", function(d,i) {return containerHeight - (d+(width/2)) + margin.left;})
-  .attr("cy", function(d,i){return i*(gap+width)+(width/2)+margin.top;})
+  .attr("cx", function(d,i) {return rangeScale(i);})
+  .attr("cy", function(d,i) {return dataScale(d);})
+  .attr("id", function(d,i) {return i;})
   .attr("r", width/2)
   .style("fill", "lightblue");
 
-  var xScale = d3.scaleLinear()
+ console.log(circleAttributes);
+ 
+  var yScale  = d3.scaleLinear()
   .domain([min, max])
-  .range([margin.left, containerWidth-margin.right]);
+  .range([containerHeight - margin.bottom, margin.top]);
 
-  var yScale = d3.scaleLinear()
+  var xScale= d3.scaleLinear()
   .domain([0, data.length])
-  .range([containerHeight-margin.top, margin.bottom]);
+  .range([margin.left, containerWidth-margin.right]);
 
   var xAxis = d3.axisBottom().scale(xScale);
   var yAxis = d3.axisLeft().scale(yScale);
 
   var xAxisGroup = svgContainer.append("g")
-  .attr("transform", "translate(0," + containerHeight + ")")
+  .attr("transform", "translate(0," + (containerHeight-margin.bottom) + ")")
   .call(xAxis);
 
   var yAxisGroup = svgContainer.append("g")
-  .attr("transform", "translate("+margin.left+","+margin.top+")")
+  .attr("transform", "translate("+margin.left+",0)")
+  .call(yAxis);
+
+  return [svgContainer, circles, circleAttributes];
+};
+
+var scatterXGraph = function(data, margin, cWidth, cHeight, g, container, landscape) {
+
+  var containerHeight = cHeight - (margin.top + margin.bottom);
+  var containerWidth = cWidth - (margin.left + margin.right);
+  var dataSize = data.length;
+  var gap = g;
+  var width = (containerHeight - (dataSize*gap))/dataSize;
+  var max, min;
+
+  console.log("Container Width: " + containerWidth);
+  console.log("Container Height: " + containerHeight);
+  console.log("Gap: " + gap, "Total Gap: " + (dataSize*gap));
+  console.log("Width: " + width, "Total width: " + (dataSize * width) );
+  console.log(margin);
+
+  for(var i = 0; i < dataSize; i++) {
+    if(max) {
+      if(max < data[i]) {
+        max = data[i];
+      }
+    } else {
+      max = data[i];
+    }
+    
+    if(min) {
+      if(min > data[i]) {
+        min = data[i];
+      }
+    } else {
+        min = data[i];
+    }
+  }
+
+  var rangeScale = d3.scaleLinear()
+    .domain([0, dataSize])
+    .range([containerHeight - (margin.bottom + width/2), margin.top+(width/2)]);
+
+  var dataScale = d3.scaleLinear()
+    .domain([min, max])
+    .range([margin.left+(width/2), containerWidth-margin.right])
+  
+  var svgContainer = d3.select(container).append("svg")
+    .attr("width", containerWidth + margin.left + margin.right)
+    .attr("height", containerHeight + margin.bottom+margin.top);
+
+  var circles = svgContainer.selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle");
+    
+  var circleAttributes = circles
+  .attr("cx", function(d,i) {return dataScale(d);})
+  .attr("cy", function(d,i) {return rangeScale(i);})
+  .attr("id", function(d,i) {return i;})
+  .attr("r", width/2)
+  .style("fill", "lightblue");
+
+ console.log(circleAttributes);
+ 
+  var yScale  = d3.scaleLinear()
+  .domain([0, data.length])
+  .range([containerHeight - margin.bottom, margin.top]);
+
+  var xScale= d3.scaleLinear()
+  .domain([min, max])
+  .range([margin.left, containerWidth-margin.right]);
+
+  var xAxis = d3.axisBottom().scale(xScale);
+  var yAxis = d3.axisLeft().scale(yScale);
+
+  var xAxisGroup = svgContainer.append("g")
+  .attr("transform", "translate(0," + (containerHeight-margin.bottom) + ")")
+  .call(xAxis);
+
+  var yAxisGroup = svgContainer.append("g")
+  .attr("transform", "translate("+margin.left+",0)")
   .call(yAxis);
 
   return [svgContainer, circles, circleAttributes];

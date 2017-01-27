f07333b844b6f5bb4d1e31b414f3858918ae4452
Andy Yu <andyyu199623@yahoo.com>
2017-01-26 23:10:10 -0500

    Display bar graph

--- chartHelper.js
+++ chartHelper.js
@@ -201,11 +201,11 @@ var barYGraph = function(data, margin, cWidth, cHeight, g, container) {
 
   var dataScale = d3.scaleLinear()
     .domain([max, min])
-    .range([containerHeight - margin.bottom, margin.top]);
+    .range([containerHeight, margin.top]);
 
   var rangeScale = d3.scaleLinear()
     .domain([0, dataSize])
-    .range([margin.left+(width/2), containerWidth-margin.right])
+    .range([margin.left, containerWidth])
   
   var svgContainer = d3.select(container).append("svg")
     .attr("width", containerWidth + margin.left + margin.right)
@@ -217,8 +217,8 @@ var barYGraph = function(data, margin, cWidth, cHeight, g, container) {
   .append("rect");
     
   var rectAttributes = rects
-  .attr("x", function(d,i) {return i*(width+gap);})
-  .attr("y", function(d,i) {return containerHeight-dataScale(d);})
+  .attr("x", function(d,i) {return rangeScale(i);})
+  .attr("y", function(d,i) {return (containerHeight+margin.top)-dataScale(d);})
   .attr("width", width)
   .attr("height", function(d,i) {return dataScale(d);})
   .attr("id", function(d,i) {return i;})
@@ -228,7 +228,7 @@ var barYGraph = function(data, margin, cWidth, cHeight, g, container) {
  
   var yScale  = d3.scaleLinear()
   .domain([min, max])
-  .range([containerHeight - margin.bottom, margin.top]);
+  .range([containerHeight+margin.top, margin.top]);
 
   var xScale= d3.scaleLinear()
   .domain([0, data.length])
@@ -238,7 +238,7 @@ var barYGraph = function(data, margin, cWidth, cHeight, g, container) {
   var yAxis = d3.axisLeft().scale(yScale);
 
   var xAxisGroup = svgContainer.append("g")
-  .attr("transform", "translate(0," + (containerHeight-margin.bottom) + ")")
+  .attr("transform", "translate(0," + (containerHeight+margin.top) + ")")
   .call(xAxis);
 
   var yAxisGroup = svgContainer.append("g")
@@ -254,7 +254,7 @@ var barXGraph = function(data, margin, cWidth, cHeight, g, container, landscape)
   var containerWidth = cWidth - (margin.left + margin.right);
   var dataSize = data.length;
   var gap = g;
-  var width = (containerHeight - (dataSize*gap))/dataSize;
+  var width = (containerWidth - (dataSize*gap))/dataSize;
   var max, min;
 
   console.log("Container Width: " + containerWidth);
@@ -281,50 +281,52 @@ var barXGraph = function(data, margin, cWidth, cHeight, g, container, landscape)
     }
   }
 
-  var rangeScale = d3.scaleLinear()
-    .domain([0, dataSize])
-    .range([containerHeight - (margin.bottom + width/2), margin.top+(width/2)]);
-
   var dataScale = d3.scaleLinear()
     .domain([min, max])
-    .range([margin.left+(width/2), containerWidth-margin.right])
+    .range([10, containerWidth])
+    
+
+  var rangeScale = d3.scaleLinear()
+    .domain([0, dataSize-1])
+    .range([containerHeight-width, margin.top]);
   
   var svgContainer = d3.select(container).append("svg")
     .attr("width", containerWidth + margin.left + margin.right)
     .attr("height", containerHeight + margin.bottom+margin.top);
 
-  var circles = svgContainer.selectAll("circle")
+  var rects = svgContainer.selectAll("rect")
   .data(data)
   .enter()
-  .append("circle");
+  .append("rect");
     
-  var circleAttributes = circles
-  .attr("cx", function(d,i) {return dataScale(d);})
-  .attr("cy", function(d,i) {return rangeScale(i);})
+  var rectAttributes = rects
+  .attr("x", margin.left)
+  .attr("y", function(d,i) {return rangeScale(i);})
+  .attr("width", function(d,i) {return dataScale(d);})
+  .attr("height", width)
   .attr("id", function(d,i) {return i;})
-  .attr("r", width/2)
   .style("fill", "lightblue");
 
- console.log(circleAttributes);
+ console.log(rectAttributes);
  
   var yScale  = d3.scaleLinear()
   .domain([0, data.length])
-  .range([containerHeight - margin.bottom, margin.top]);
+  .range([containerHeight+margin.top, margin.top]);
 
   var xScale= d3.scaleLinear()
   .domain([min, max])
-  .range([margin.left, containerWidth-margin.right]);
+  .range([margin.left, containerWidth]);
 
   var xAxis = d3.axisBottom().scale(xScale);
   var yAxis = d3.axisLeft().scale(yScale);
 
   var xAxisGroup = svgContainer.append("g")
-  .attr("transform", "translate(0," + (containerHeight-margin.bottom) + ")")
+  .attr("transform", "translate(0," + (containerHeight+margin.top) + ")")
   .call(xAxis);
 
   var yAxisGroup = svgContainer.append("g")
   .attr("transform", "translate("+margin.left+",0)")
   .call(yAxis);
 
-  return [svgContainer, circles, circleAttributes];
+  return [svgContainer, rects, rectAttributes];
 };
